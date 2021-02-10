8b1ded46b0732c911c0aa780758784da8a2f61c0
Arbab Ahmed <arby3k@gmail.com>
2021-02-10 14:54:02 -0800

    Updated UI test for GeoSearch

--- app/src/androidTest/java/com/example/photoapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/photoapp/ExampleInstrumentedTest.java
@@ -18,22 +18,22 @@ public class ExampleInstrumentedTest {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
     @Test
     public void ensureTextChangesWork() {
-        //Automated UI Test for search Activity
-        onView(withId(R.id.filter)).perform(click());
-        //onView(withId(R.id.etlongitude)).perform(replaceText(""), closeSoftKeyboard());
-        onView(withId(R.id.go)).perform(click());
-       // onView(withId(R.id.etlongitude)).check(matches(withText("")));
-
+        //Show the photos in app
         onView(withId(R.id.nextid)).perform(click());
         onView(withId(R.id.nextid)).perform(click());
         onView(withId(R.id.previd)).perform(click());
         onView(withId(R.id.previd)).perform(click());
 
+        //Open Search Activity
         onView(withId(R.id.filter)).perform(click());
-        //onView(withId(R.id.etlatitude)).perform(replaceText(""), closeSoftKeyboard());
+        //Type in Search criteria
+        onView(withId(R.id.latitudeid)).perform(replaceText("80"), closeSoftKeyboard());
+        onView(withId(R.id.longitudeid)).perform(replaceText("-110"), closeSoftKeyboard());
+        onView(withId(R.id.latitudeid2)).perform(replaceText("90"), closeSoftKeyboard());
+        onView(withId(R.id.longitudeid2)).perform(replaceText("-90"), closeSoftKeyboard());
         onView(withId(R.id.go)).perform(click());
-        //onView(withId(R.id.etlatitude)).check(matches(withText("")));
 
+        //check to see if any other photos show up
         onView(withId(R.id.nextid)).perform(click());
         onView(withId(R.id.nextid)).perform(click());
         onView(withId(R.id.previd)).perform(click());
